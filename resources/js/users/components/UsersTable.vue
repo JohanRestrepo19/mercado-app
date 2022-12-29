2d10120db7ef98eb7f1345fe22f5c589deba3938
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2022-12-29 08:09:11 -0500

    fix(users): Responsive users table.

--- resources/js/users/components/UsersTable.vue
+++ resources/js/users/components/UsersTable.vue
@@ -1,71 +1,77 @@
 <template>
-    <table class="table table-responsive" id="usersTable">
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>Cédula</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Email</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="user in usersArr" :key="user.id">
-                <td>{{ user.id }}</td>
-                <td>{{ user.number_id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.last_name }}</td>
-                <td>{{ user.email }}</td>
-                <td class="d-flex">
-                    <a class="btn btn-info mx-2" :href="`/users/${user.id}/edit`">Editar</a>
-                    <button @click="handleClickDelete(user.id)" type="button" class="btn btn-danger mx-2">
-                        Eliminar
-                    </button>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="table-responsive">
+        <table class="table" id="usersTable">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Cédula</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in usersArr" :key="user.id">
+                    <td>{{ user.id }}</td>
+                    <td>{{ user.number_id }}</td>
+                    <td>{{ user.name }}</td>
+                    <td>{{ user.last_name }}</td>
+                    <td>{{ user.email }}</td>
+                    <td class="d-flex">
+                        <a class="btn btn-info mx-2" :href="`/users/${user.id}/edit`">Editar</a>
+                        <button
+                            @click="handleClickDelete(user.id)"
+                            type="button"
+                            class="btn btn-danger mx-2"
+                        >
+                            Eliminar
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-import DataTable from 'datatables.net-bs5'
-import axios from 'axios'
-import { showConfirmationAlert } from '../../utils/notifications'
+    import DataTable from 'datatables.net-bs5'
+    import axios from 'axios'
+    import { showConfirmationAlert } from '../../utils/notifications'
 
-export default {
-    data() {
-        return {
-            usersArr: [],
-            dataTable: null
-        }
-    },
-    props: ['users'],
-    methods: {
-        mountDataTable() {
-            this.$nextTick(() => {
-                this.dataTable = new DataTable('#usersTable')
-            })
-        },
-        async handleClickDelete(userId) {
-            const confirmation = await showConfirmationAlert()
-            if (confirmation) {
-                this.usersArr = this.usersArr.filter(user => user.id !== userId)
-                this.deleteUserRequest(userId)
+    export default {
+        data() {
+            return {
+                usersArr: [],
+                dataTable: null
             }
-            return
         },
-        async deleteUserRequest(userId) {
-            try {
-                await axios.delete(`/users/delete/${userId}`)
-            } catch (error) {
-                console.error(error)
+        props: ['users'],
+        methods: {
+            mountDataTable() {
+                this.$nextTick(() => {
+                    this.dataTable = new DataTable('#usersTable')
+                })
+            },
+            async handleClickDelete(userId) {
+                const confirmation = await showConfirmationAlert()
+                if (confirmation) {
+                    this.usersArr = this.usersArr.filter(user => user.id !== userId)
+                    this.deleteUserRequest(userId)
+                }
+                return
+            },
+            async deleteUserRequest(userId) {
+                try {
+                    await axios.delete(`/users/delete/${userId}`)
+                } catch (error) {
+                    console.error(error)
+                }
             }
+        },
+        mounted() {
+            this.usersArr = this.users
+            this.mountDataTable()
         }
-    },
-    mounted() {
-        this.usersArr = this.users
-        this.mountDataTable()
     }
-}
 </script>
